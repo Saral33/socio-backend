5f241c9cbde6fcba5aefbbba8ac579479b1d8a6a
Saral33 <princesaral2013@gmail.com>
2024-06-04 14:21:01 +0545

    init

---
+++ src/shared/services/redis/base.cache.ts
@@ -0,0 +1,27 @@
+import { createClient } from 'redis';
+import Logger from 'bunyan';
+import { config } from '@root/config';
+
+export type RedisClient = ReturnType<typeof createClient>;
+
+export abstract class BaseCache {
+  client: RedisClient;
+  log: Logger;
+
+  constructor(cacheName: string) {
+    this.client = createClient({
+      socket: {
+        port: Number(config.REDIS_PORT),
+        host: config.REDIS_HOST
+      }
+    });
+    this.log = config.createLogger(cacheName);
+    this.cacheError();
+  }
+
+  private cacheError(): void {
+    this.client.on('error', (error: unknown) => {
+      this.log.error(error);
+    });
+  }
+}
